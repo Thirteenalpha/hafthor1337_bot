f5d5b7d23efd6b6667b0be93c0f3ec0690c412a6
FaizanHaider20083 <faizan20083@iiitd.ac.in>
2021-03-03 16:11:11 +0530

    first commit

---
+++ reddit_50.py
@@ -0,0 +1,65 @@
+import praw
+from praw.models import Message
+import time 
+import schedule
+
+
+
+reddit = praw.Reddit(client_id = 'iYrHnMb2wlcLmg',
+client_secret = 'K3ZsTAIUAfDZN6ceeO0GL9KPrGVT2Q',
+user_agent = 'console: message_bot 1.0',
+username = 'ConcentrateOk3302',
+password = '7371+10270')
+
+subreddits = ['tes_ting']
+
+
+
+whatcarshouldIbuy_count = 0
+def whatcarshouldIbuy_count_reset():
+        global whatcarshouldIbuy_count
+        whatcarshouldIbuy_count = 0
+
+schedule.every().day.at("00:00").do(whatcarshouldIbuy_count_reset)
+
+def check_author(author):
+    
+    file_read = open("Message_list.txt",'r+')
+    
+    for line in file_read.readlines():
+        
+        if line == (author + '\n'):
+            file_read.close()
+            
+            return 1
+    file_read.close()
+    
+    return 0
+
+
+
+
+i = 50
+while (i>0):
+        message_list = open("Message_list.txt",'a+')
+        for topic in subreddits:
+                
+                subreddit = reddit.subreddit(topic)
+                for submission in subreddit.new(limit = 20):
+                        
+                        author = str(submission.author)
+                        
+                        if check_author(author) == 1 :
+                        
+                                continue
+                        if (topic == 'whatcarshouldIbuy') and (whatcarshouldIbuy_count < 5):
+                                reddit.redditor(author).message('Cheap Carfax','Get your $5 carfax here: https://carsimulcast.com/')
+                                whatcarshouldIbuy_count += 1
+                                message_list.write('\n' + author)
+                                continue
+                        reddit.redditor(author).message('Cheap Carfax','Get your $5 carfax here: https://carsimulcast.com/')
+                        message_list.write('\n'+ author)
+                        print("here")
+        message_list.close()
+        print("hello")
+        time.sleep(5)       
